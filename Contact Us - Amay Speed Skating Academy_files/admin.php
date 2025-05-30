649ae494cfc00fe437cb6ab47e8f818028db9a98
Arshad Qureshi <arshad@ennovatorsllc.com>
2025-05-30 09:41:35 -0500

    Initial commit

---
+++ Contact Us - Amay Speed Skating Academy_files/admin.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+	<html lang="en-US">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Log In &lsaquo; Amay Speed Skating Academy &#8212; WordPress</title>
+	<meta name='robots' content='noindex, follow' />
+<link rel='preconnect' href='//i0.wp.com' />
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js"></script>
+<link rel='stylesheet' id='dashicons-css' href='https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/css/dashicons.min.css?ver=6.8.1' media='all' />
+<link rel='stylesheet' id='buttons-css' href='https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/css/buttons.min.css?ver=6.8.1' media='all' />
+<link rel='stylesheet' id='forms-css' href='https://skh.mlg.mybluehost.me/website_c004324e/wp-admin/css/forms.min.css?ver=6.8.1' media='all' />
+<link rel='stylesheet' id='l10n-css' href='https://skh.mlg.mybluehost.me/website_c004324e/wp-admin/css/l10n.min.css?ver=6.8.1' media='all' />
+<link rel='stylesheet' id='login-css' href='https://skh.mlg.mybluehost.me/website_c004324e/wp-admin/css/login.min.css?ver=6.8.1' media='all' />
+<link rel='stylesheet' id='nfd-secure-passwords-login-css' href='https://skh.mlg.mybluehost.me/website_c004324e/wp-content/plugins/bluehost-wordpress-plugin/vendor/newfold-labs/wp-module-secure-passwords/assets/css/login.css?ver=1.0.3' media='all' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script>
+document.body.className = document.body.className.replace('no-js','js');
+</script>
+
+				<h1 class="screen-reader-text">Log In</h1>
+			<div id="login">
+		<h1 role="presentation" class="wp-login-logo"><a href="https://wordpress.org/">Powered by WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="https://skh.mlg.mybluehost.me/website_c004324e/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="ARSHAD.QURESHIH" size="20" autocapitalize="off" autocomplete="username" required="required" />
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false" required="required" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="https://skh.mlg.mybluehost.me/website_c004324e/wp-admin/admin.php?page=stats&amp;noheader&amp;proxy&amp;chart=admin-bar-hours-scale" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
+
+					<p id="nav">
+				<a class="wp-login-lost-password" href="https://skh.mlg.mybluehost.me/website_c004324e/wp-login.php?action=lostpassword">Lost your password?</a>			</p>
+			<script>
+function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_pass" ); d.value = "";d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }
+</script>
+		<p id="backtoblog">
+			<a href="https://skh.mlg.mybluehost.me/website_c004324e/">&larr; Go to Amay Speed Skating Academy</a>		</p>
+			</div>
+		
+	<script id="ce4wp_form_submit-js-extra">
+var ce4wp_form_submit_data = {"siteUrl":"https:\/\/skh.mlg.mybluehost.me\/website_c004324e","url":"https:\/\/skh.mlg.mybluehost.me\/website_c004324e\/wp-admin\/admin-ajax.php","nonce":"b63fb0e457","listNonce":"6c2b0cef42","activatedNonce":"05ca2925db"};
+</script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-content/plugins/creative-mail-by-constant-contact/assets/js/block/submit.js?ver=1746083294" id="ce4wp_form_submit-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/underscore.min.js?ver=1.13.7" id="underscore-js"></script>
+<script id="wp-util-js-extra">
+var _wpUtilSettings = {"ajax":{"url":"\/website_c004324e\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/wp-util.min.js?ver=6.8.1" id="wp-util-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-content/plugins/bluehost-wordpress-plugin/vendor/newfold-labs/wp-module-secure-passwords/assets/js/secure-passwords.js?ver=1.0.3" id="nfd-secure-passwords-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/clipboard.min.js?ver=2.0.11" id="clipboard-js"></script>
+<script id="zxcvbn-async-js-extra">
+var _zxcvbnSettings = {"src":"https:\/\/skh.mlg.mybluehost.me\/website_c004324e\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/dist/hooks.min.js?ver=4d63a3d491d11ffd8ac6" id="wp-hooks-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/dist/i18n.min.js?ver=5e580eb46a90c2b997e6" id="wp-i18n-js"></script>
+<script id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text direction\u0004ltr': [ 'ltr' ] } );
+</script>
+<script id="password-strength-meter-js-extra">
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+</script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-admin/js/password-strength-meter.min.js?ver=6.8.1" id="password-strength-meter-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/dist/dom-ready.min.js?ver=f77871ff7694fffea381" id="wp-dom-ready-js"></script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-includes/js/dist/a11y.min.js?ver=3156534cc54473497e14" id="wp-a11y-js"></script>
+<script id="user-profile-js-extra">
+var userProfileL10n = {"user_id":"0","nonce":"905e2344d3"};
+</script>
+<script src="https://skh.mlg.mybluehost.me/website_c004324e/wp-admin/js/user-profile.min.js?ver=6.8.1" id="user-profile-js"></script>
+	</body>
+	</html>
+	
